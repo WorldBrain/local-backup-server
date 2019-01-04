ba52f9dd30f2042a058c2d2d1e34ceff42e21c34
Eik-S <eikem@posteo.de>
2019-01-04 17:32:29 +0100

    first koa server version finished

--- HTTPKoaServer.js
+++ HTTPKoaServer.js
@@ -1,10 +1,11 @@
 var fs = require('fs')
+var mkdirp = require('mkdirp')
 var Koa = require('koa')
 var Router = require('koa-router')
 var bodyparser = require('koa-bodyparser')
-let app = new Koa()
-let router = new Router({ prefix: '' })
 
+let app = new Koa()
+let router = new Router({ prefix: '/backup' })
 const port = 11922
 
 // error handling
@@ -22,6 +23,7 @@ app.use(async (ctx, next) => {
 app.use(
     bodyparser({
         enableTypes: ['text'],
+        textLimit: '50mb',
         onerror: function(err, ctx) {
             console.log(err.message)
             ctx.throw('body parse error', 422)
@@ -30,13 +32,48 @@ app.use(
 )
 
 // putting new files
-router.put('/change-sets/:date', (ctx, next) => {
-    const filename = ctx.params.date
-    const path = process.cwd() + '/' + filename
-    fs.writeFile(path, ctx.request.body, function(err) {
+router.put('/:collection/:timestamp', (ctx, next) => {
+    const filename = ctx.params.timestamp
+    const collection = ctx.params.collection
+    const dirpath = process.cwd() + `/backup/${collection}`
+    mkdirp(dirpath, function(err) {
         if (err) throw err
+        const filepath = dirpath + `/${filename}`
+        fs.writeFile(filepath, ctx.request.body, function(err) {
+            if (err) throw err
+        })
     })
     ctx.status = 200
+})
+
+// getting files
+router.get('/:collection/:timestamp', (ctx, next) => {
+    const filename = ctx.params.timestamp
+    const collection = ctx.params.collection
+    const filepath = process.cwd() + `/backup/${collection}/` + filename
+    try {
+        ctx.body = fs.readFileSync(filepath, 'utf-8')
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            ctx.status = 404
+            ctx.body = 'File not found.'
+        } else throw err
+    }
+})
+
+// listing files
+router.get('/:collection', (ctx, next) => {
+    const collection = ctx.params.collection
+    const dirpath = process.cwd() + `/backup/${collection}`
+    try { 
+        const filelist = fs.readdirSync(dirpath, 'utf-8')
+        ctx.body = filelist.toString()
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            ctx.status = 404
+            ctx.body = 'Collection not found.'
+        } else throw err
+    }
 })
 
 app.use(router.routes())
